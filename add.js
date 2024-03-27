7c502382a999a338d02835fcf4e177ee03e7890a
MastourKh <161030088+MastourKh@users.noreply.github.com>
2024-03-27 23:52:30 +0300

    Create add.js

---
+++ add.js
@@ -0,0 +1,7 @@
+const functions = require('@google-cloud/functions-framework');
+
+functions.http('add', (req, res) => {
+  const { num1, num2 } = req.body;
+  const result = num1 + num2;
+  res.status(200).json({ result });
+});
